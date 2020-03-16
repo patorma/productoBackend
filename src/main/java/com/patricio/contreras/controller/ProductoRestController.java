8a0a756ffb107916489b003a6f7df96c4066bffe
patorma <pocontreras@ing.ucsc.cl>
2020-03-15 23:12:45 -0300

    se hicieron correcciones en el controller

--- src/main/java/com/patricio/contreras/controller/ProductoRestController.java
+++ src/main/java/com/patricio/contreras/controller/ProductoRestController.java
@@ -1,5 +1,6 @@
 package com.patricio.contreras.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoRestController {
 
 	@Autowired
